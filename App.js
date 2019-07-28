9f9cc5b3ccfeda26c03ddc8e1be0f682e12a9ad3
Nguyen Trong <trong.nguyen@vred.vn>
2019-07-28 23:28:39 +0700

    update Profile App

--- App.js
+++ App.js
@@ -7,16 +7,19 @@
  */
 
 import React, { Fragment } from 'react';
+import IconIonicons from 'react-native-vector-icons/Ionicons'
 import {
   StyleSheet,
   View,
   Text,
   Image,
-  Button
+  Button,
+  FlatList,
+  ScrollView
 } from 'react-native';
 
 import {
- 
+
 } from 'react-native/Libraries/NewAppScreen';
 const distanceLayout = 2
 const App = () => {
@@ -65,26 +68,26 @@ const App = () => {
           <View style={{
             flexDirection: 'row',
             flex: 0.2,
-          }}> 
+          }}>
             <View style={{
-              backgroundColor:'blue',
+              backgroundColor: 'blue',
               width: 100,
               borderRadius: 20,
               justifyContent: 'center',
               alignItems: 'center',
-              
+
             }}>
-              <Button title="Follow" color="white"/>
+              <Button title="Follow" color="white" />
             </View>
             <View style={{
-              marginLeft:10,
-              backgroundColor:'#3399FF',
+              marginLeft: 10,
+              backgroundColor: '#3399FF',
               width: 60,
               borderRadius: 20,
               justifyContent: 'center',
               alignItems: 'center'
             }}>
-              <Button title=">>" color="white"/>
+              <IconIonicons name="md-send" size={20} style={{ color: 'white' }} />
             </View>
 
           </View>
@@ -92,15 +95,77 @@ const App = () => {
       </View>
       <View style={{
         flex: 0.1,
-        backgroundColor: 'blue'
+        flexDirection: 'row',
+        justifyContent: 'center',
+        // alignItems: 'center'
       }}>
-
+        <View style={{
+          justifyContent: 'center',
+          padding: distanceLayout,
+          flexDirection: 'column',
+          flex: 0.8,
+          alignItems: 'center'
+        }}>
+          <Text style={{
+            fontSize: 20,
+            padding: distanceLayout,
+            fontWeight: 'bold',
+          }}>210</Text>
+          <Text style={{
+            padding: distanceLayout,
+          }}>Photos</Text>
+        </View>
+        <View style={{
+          justifyContent: 'center',
+          padding: distanceLayout,
+          flexDirection: 'column',
+          flex: 0.8,
+          alignItems: 'center'
+        }}>
+          <Text style={{
+            fontSize: 20,
+            padding: distanceLayout,
+            fontWeight: 'bold'
+          }}>15k</Text>
+          <Text style={{
+            padding: distanceLayout,
+          }}>Followers</Text>
+        </View>
+        <View style={{
+          justifyContent: 'center',
+          padding: distanceLayout,
+          flexDirection: 'column',
+          flex: 0.8,
+          alignItems: 'center'
+        }}>
+          <Text style={{
+            fontSize: 20,
+            padding: distanceLayout,
+            fontWeight: 'bold'
+          }}>605</Text>
+          <Text style={{
+            padding: distanceLayout,
+          }}>Following</Text>
+        </View>
       </View>
+
       <View style={{
         flex: 0.7,
-        backgroundColor: 'yellow'
+        margin: 20,
       }}>
-        
+        <FlatList
+          data={[{ key: '1' }, { key: '2' }, { key: '3' }, { key: '4' }
+          ,{ key: '5' }, { key: '6' }, { key: '7' }, { key: '8' }, {key: '9'}
+        ]}
+          renderItem={({ item }) => {
+            return <Image
+              style={{ width: 130, height: 130, justifyContent: 'center', borderRadius: 20, margin: 10 }}
+              source={{ uri: 'http://thuthuatphanmem.vn/uploads/2018/09/11/hinh-anh-dep-10_044127763.jpg' }}
+            />
+
+          }}
+          numColumns = {2}
+        />
       </View>
     </View>
 
--- index.js
+++ index.js
@@ -9,4 +9,4 @@ import {name as appName} from './app.json';
 
 // AppRegistry.registerComponent(appName, () => App);
 
-AppRegistry.registerComponent(appName, () => Home);
+AppRegistry.registerComponent(appName, () => App);
